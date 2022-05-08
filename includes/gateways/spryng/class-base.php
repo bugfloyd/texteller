141bd90bae3a114b8d03a8222582488e6005bf3a
“Yashar <yashar.hv@gmail.com>
2022-05-09 00:06:02 +0200

    [FEAT] Spryng gateway: add send and delivery callback

--- includes/gateways/spryng/class-base.php
+++ includes/gateways/spryng/class-base.php
@@ -2,14 +2,9 @@
 
 namespace Texteller\Gateways\Spryng;
 
-use Spryng\SpryngRestApi\Spryng;
-
 use Texteller as TLR;
-use Twilio\Exceptions\ConfigurationException;
-use Twilio\Exceptions\TwilioException;
-use Twilio\Rest\Api\V2010\AccountInstance;
-use Twilio\Rest\Client;
-use Twilio\Security\RequestValidator;
+use Spryng\SpryngRestApi\Spryng;
+use Spryng\SpryngRestApi\Objects\Message;
 use WP_Error;
 use WP_REST_Request;
 
@@ -22,12 +17,12 @@ class Base implements TLR\Interfaces\Gateway
     use TLR\Traits\Encrypted_Options;
 
     /**
-     * @var null|Client $client Twilio Client
+     * @var null|Spryng $client Spryng Client
      */
     private static ?Spryng $client = null;
 
     /**
-     * @var null|AccountInstance $account
+     * @var null|string $balance
      */
     private static ?string $balance = null;
 
@@ -42,68 +37,29 @@ class Base implements TLR\Interfaces\Gateway
         }
 
         if (!self::get_client()) {
-            return '';
+            return "";
         }
 
-	    $balance_object = self::get_client()->balance->get()->toObject();
-	    self::$balance = $balance_object->getAmount();
+        $balance_object = self::get_client()
+            ->balance->get()
+            ->toObject();
+        self::$balance = $balance_object->getAmount();
         return (string) self::$balance;
     }
 
     public static function get_account_details()
     {
         $balance = self::get_balance();
-        if ($balance === null | $balance === '') {
+        if (($balance === null) | ($balance === "")) {
             return false;
         }
 
-//        $date_created = empty($account->dateCreated)
-//            ? $account->dateCreated
-//            : "";
-//
-//        if ($date_created) {
-//            self::init_datetime_formatter();
-//            $date_created = self::format_datetime($date_created);
-//        }
-//
-//        $friendly_name = empty($account->friendlyName)
-//            ? $account->friendlyName
-//            : "";
-//        $status = empty($account->status) ? $account->status : "";
-//        $type = empty($account->type) ? $account->type : "";
-//
-//        $balance = "";
-//        if (empty($account->subresourceUris["balance"])) {
-//            TLR\tlr_write_log(
-//                "Twilio: Cannot get account balance subsource URI"
-//            );
-//        } else {
-//            $balanceUrl =
-//                "https://api.twilio.com" . $account->subresourceUris["balance"];
-//            $balanceResponse = self::$client->request("GET", $balanceUrl);
-//            $responseContent = $balanceResponse->getContent();
-//            if (
-//                !isset($responseContent["balance"]) ||
-//                !isset($responseContent["currency"])
-//            ) {
-//                TLR\tlr_write_log("Twilio: Cannot get account balance details");
-//            } else {
-//                $balance =
-//                    round($responseContent["balance"], 2) .
-//                    " " .
-//                    $responseContent["currency"];
-//            }
-//        }
         return [
-//            "friendly_name" => $friendly_name,
-//            "date_created" => $date_created,
-//            "status" => $status,
-//            "type" => $type,
             "balance" => $balance,
         ];
     }
 
-    public static function get_client()
+    public static function get_client(): ?Spryng
     {
         if (self::$client && is_object(self::$client)) {
             return self::$client;
@@ -114,8 +70,8 @@ class Base implements TLR\Interfaces\Gateway
             return null;
         }
 
-	    self::$client = new Spryng($token);
-	    return self::$client;
+        self::$client = new Spryng($token);
+        return self::$client;
     }
 
     public function send(
@@ -128,10 +84,10 @@ class Base implements TLR\Interfaces\Gateway
             return false;
         }
 
-        if ("twilio-sms" === $interface) {
+        if ("spryng-sms" === $interface) {
             return $this->send_sms($text, $number);
         } else {
-            return false; // todo
+            return false;
         }
     }
 
@@ -140,24 +96,29 @@ class Base implements TLR\Interfaces\Gateway
         if (null === self::$client || !is_object(self::$client)) {
             return false;
         }
-        try {
-            $MessageInstance = self::$client->messages->create($number, [
-                "from" => get_option("tlr_gateway_twilio_number"),
-                "body" => $text,
-                "statusCallback" => str_replace(
-                    "http://localhost",
-                    "https://c36d-83-83-117-146.eu.ngrok.io",
-                    get_rest_url(null, "texteller/v1/delivery/twilio/")
-                ),
-            ]);
-            $message_sid = $MessageInstance->sid;
-            return $message_sid ? ["sid" => $message_sid] : false;
-        } catch (TwilioException $e) {
+
+        $message = new Message();
+        $message->setBody($text);
+        $message->setRecipients([$number]);
+        $message->setOriginator("Texteller");
+
+        $response = self::$client->message->create($message);
+
+        if ($response->wasSuccessful()) {
+            $message = $response->toObject();
+            $message_id = $message->getId();
+            return $message_id ? ["id" => $message_id] : false;
+        } elseif ($response->serverError()) {
             TLR\tlr_write_log(
-                "Twilio: An error occurred. | " . $e->getMessage()
+                "Spryng: An error occurred while sending the message on Spryng servers."
             );
-            return false;
+        } else {
+            TLR\tlr_write_log(
+                "Message could not be send. Response code: " .
+                    $response->getResponseCode()
+            );
         }
+        return false;
     }
 
     /**
@@ -165,116 +126,115 @@ class Base implements TLR\Interfaces\Gateway
      *
      * @return string|WP_Error
      */
-    public static function rest_delivery_callback(WP_REST_Request $request)
-    {
-        $method = $request->get_method();
-        $delivery_body = $request->get_body_params();
-        if (
-            "POST" === $method &&
-            !empty($delivery_body["MessageSid"]) &&
-            !empty($delivery_body["MessageStatus"]) &&
-            in_array(
-                $delivery_body["MessageStatus"],
-                ["sent", "delivered", "read", "failed"],
-                true
-            )
-        ) {
-            $args = [
-                "object_type" => "message",
-                "statuses" => ["sent", "delivered", "failed", "pending"],
-                "gateways" => ["twilio"],
-                "field" => "ID",
-                "gateway_data" => sanitize_text_field(
-                    $delivery_body["MessageSid"]
-                ),
-            ];
-            $message_query = new TLR\Object_Query($args);
-            $message_id = $message_query->get_messages(1);
-            $message = new TLR\Message($message_id[0]);
-            if ($message->get_id()) {
-                $message->set_status($delivery_body["MessageStatus"]);
-                $message->save();
-                return "success";
+        public static function rest_delivery_callback(WP_REST_Request $request)
+        {
+            $method = $request->get_method();
+            $delivery_body = $request->get_body_params();
+            if (
+                "GET" === $method &&
+                !empty($delivery_body["ID"]) &&
+                !empty($delivery_body["STATUS"]) &&
+                in_array(
+                    $delivery_body["STATUS"],
+                    [10, 20]
+                )
+            ) {
+                $args = [
+                    "object_type" => "message",
+                    "statuses" => ["sent", "delivered", "failed", "pending"],
+                    "gateways" => ["spryng"],
+                    "field" => "ID",
+                    "gateway_data" => sanitize_text_field(
+                        $delivery_body["ID"]
+                    ),
+                ];
+                $message_query = new TLR\Object_Query($args);
+                $message_id = $message_query->get_messages(1);
+                $message = new TLR\Message($message_id[0]);
+                if ($message->get_id()) {
+                    $message->set_status($delivery_body["STATUS"] === 10 ? 'delivered' : 'failed');
+                    $message->save();
+                    return "success";
+                }
             }
-        }
-        return new WP_Error(
-            "rest_invalid_message_data",
-            esc_html("Invalid message data"),
-            ["status" => 404]
-        );
-    }
-
-    /**
-     * @param WP_REST_Request $request Current request.
-     *
-     * @return string|WP_Error
-     */
-    public static function rest_receive_callback(WP_REST_Request $request)
-    {
-        $method = $request->get_method();
-        $message_body = $request->get_body_params();
-        $message_sid = isset($message_body["MessageSid"])
-            ? sanitize_text_field($message_body["MessageSid"])
-            : "";
-        $text = isset($message_body["Body"])
-            ? sanitize_text_field($message_body["Body"])
-            : "";
-        $to = isset($message_body["To"])
-            ? sanitize_text_field($message_body["To"])
-            : "";
-        $from = isset($message_body["From"])
-            ? sanitize_text_field($message_body["From"])
-            : "";
-        $member_id = TLR\tlr_get_member_id($from);
-
-        if ("POST" !== $method || !$from || !$to || !$message_sid) {
             return new WP_Error(
                 "rest_invalid_message_data",
                 esc_html("Invalid message data"),
                 ["status" => 404]
             );
-        } else {
-            if ($to !== self::get_interface_number("twilio-sms")) {
-                return new WP_Error(
-                    "rest_invalid_inbound_number",
-                    esc_html("Invalid inbound phone number"),
-                    ["status" => 404]
-                );
-            }
-            $token = self::get_encrypted_option(
-                "tlr_gateway_twilio_auth_token"
-            );
-            $url = get_rest_url(null, "texteller/v1/receive/twilio/");
-            $validator = new RequestValidator($token);
-
-            if (
-                $validator->validate(
-                    $request->get_header("x-twilio-signature"),
-                    $url,
-                    $message_body
-                )
-            ) {
-                $message = new TLR\Message();
-                $message->set_recipient($from);
-                $message->set_gateway("twilio");
-                $message->set_interface("twilio-sms");
-                $message->set_interface_number($to);
-                $message->set_gateway_data(["sid" => $message_sid]);
-                $message->set_content($text);
-                $message->set_status("received");
-                $message->set_trigger("tlr_inbound_message");
-                $message->set_member_id($member_id);
-                $message->save();
-                return "success";
-            } else {
-                return new WP_Error(
-                    "rest_forbidden_request",
-                    esc_html("Forbidden"),
-                    ["status" => 403]
-                );
-            }
         }
-    }
+
+//    /**
+//     * @param WP_REST_Request $request Current request.
+//     *
+//     * @return string|WP_Error
+//     */
+    //    public static function rest_receive_callback(WP_REST_Request $request)
+    //    {
+    //        $method = $request->get_method();
+    //        $message_body = $request->get_body_params();
+    //        $message_sid = isset($message_body["MessageSid"])
+    //            ? sanitize_text_field($message_body["MessageSid"])
+    //            : "";
+    //        $text = isset($message_body["Body"])
+    //            ? sanitize_text_field($message_body["Body"])
+    //            : "";
+    //        $to = isset($message_body["To"])
+    //            ? sanitize_text_field($message_body["To"])
+    //            : "";
+    //        $from = isset($message_body["From"])
+    //            ? sanitize_text_field($message_body["From"])
+    //            : "";
+    //        $member_id = TLR\tlr_get_member_id($from);
+    //
+    //        if ("POST" !== $method || !$from || !$to || !$message_sid) {
+    //            return new WP_Error(
+    //                "rest_invalid_message_data",
+    //                esc_html("Invalid message data"),
+    //                ["status" => 404]
+    //            );
+    //        } else {
+    //            if ($to !== self::get_interface_number("twilio-sms")) {
+    //                return new WP_Error(
+    //                    "rest_invalid_inbound_number",
+    //                    esc_html("Invalid inbound phone number"),
+    //                    ["status" => 404]
+    //                );
+    //            }
+    //            $token = self::get_encrypted_option(
+    //                "tlr_gateway_twilio_auth_token"
+    //            );
+    //            $url = get_rest_url(null, "texteller/v1/receive/twilio/");
+    //            $validator = new RequestValidator($token);
+    //
+    //            if (
+    //                $validator->validate(
+    //                    $request->get_header("x-twilio-signature"),
+    //                    $url,
+    //                    $message_body
+    //                )
+    //            ) {
+    //                $message = new TLR\Message();
+    //                $message->set_recipient($from);
+    //                $message->set_gateway("twilio");
+    //                $message->set_interface("twilio-sms");
+    //                $message->set_interface_number($to);
+    //                $message->set_gateway_data(["sid" => $message_sid]);
+    //                $message->set_content($text);
+    //                $message->set_status("received");
+    //                $message->set_trigger("tlr_inbound_message");
+    //                $message->set_member_id($member_id);
+    //                $message->save();
+    //                return "success";
+    //            } else {
+    //                return new WP_Error(
+    //                    "rest_forbidden_request",
+    //                    esc_html("Forbidden"),
+    //                    ["status" => 403]
+    //                );
+    //            }
+    //        }
+    //    }
 
     public static function get_interfaces(): array
     {
@@ -290,7 +250,7 @@ class Base implements TLR\Interfaces\Gateway
 
     public static function get_interface_number(string $interface): string
     {
-        return '';
+        return "";
     }
 
     public static function get_content_types(): array
@@ -326,7 +286,6 @@ class Base implements TLR\Interfaces\Gateway
             10,
             2
         );
-
     }
 
     public static function add_options()
@@ -349,12 +308,15 @@ class Base implements TLR\Interfaces\Gateway
                 "title" => __("Spryng API key", "texteller"),
                 "page" => "tlr_gateways",
                 "section" => "tlr_gateway_spryng",
-                "desc" => __("Spryng account SID acquired from portal.spryngsms.com", 'texteller'),
+                "desc" => __(
+                    "Spryng account SID acquired from portal.spryngsms.com",
+                    "texteller"
+                ),
                 "type" => "input",
                 "params" => [
                     "type" => "password",
                 ],
-            ]
+            ],
         ];
         self::register_options($options);
     }
@@ -386,40 +348,6 @@ class Base implements TLR\Interfaces\Gateway
     esc_html_e("Please save valid authentication credentials", "texteller");
 } ?></span>
             </div><?php if ($account_details) { ?>
-<!--                <div class="gateway-info-label-wrap">-->
-<!--                    <span>--><?//= esc_html__(
-//                        "Account Friendly Name",
-//                        "texteller"
-//                    ) ?><!--</span>-->
-<!--                </div>-->
-<!--                <div class="gateway-info-value-wrap">-->
-<!--                    <span>--><?//= esc_html(
-//                        $account_details["friendly_name"]
-//                    ) ?><!--</span>-->
-<!--                </div>-->
-<!--                <div class="gateway-info-label-wrap">-->
-<!--                    <span>--><?//= esc_html__("Date Created", "texteller") ?><!--</span>-->
-<!--                </div>-->
-<!--                <div class="gateway-info-value-wrap">-->
-<!--                    <span>--><?//= esc_html(
-//                        $account_details["date_created"]
-//                    ) ?><!--</span>-->
-<!--                </div>-->
-<!--                <div class="gateway-info-label-wrap">-->
-<!--                    <span>--><?//= esc_html__(
-//                        "Account Status",
-//                        "texteller"
-//                    ) ?><!--</span>-->
-<!--                </div>-->
-<!--                <div class="gateway-info-value-wrap">-->
-<!--                    <span>--><?//= esc_html($account_details["status"]) ?><!--</span>-->
-<!--                </div>-->
-<!--                <div class="gateway-info-label-wrap">-->
-<!--                    <span>--><?//= esc_html__("Account Type", "texteller") ?><!--</span>-->
-<!--                </div>-->
-<!--                <div class="gateway-info-value-wrap">-->
-<!--                    <span>--><?//= esc_html($account_details["type"]) ?><!--</span>-->
-<!--                </div>-->
                 <div class="gateway-info-label-wrap">
                     <span><?= esc_html__("Balance", "texteller") ?></span>
                 </div>
