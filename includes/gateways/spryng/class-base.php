0d5d96a8ab87961d0a02c4b20b4ce9627ccac13c
“Yashar <yashar.hv@gmail.com>
2022-05-11 02:00:08 +0200

    [FEAT] Spryng: Add delivery callback and originator & route options

--- includes/gateways/spryng/class-base.php
+++ includes/gateways/spryng/class-base.php
@@ -97,17 +97,27 @@ class Base implements TLR\Interfaces\Gateway
             return false;
         }
 
+        $originator = self::get_originator();
+        $route = self::get_route();
+
         $message = new Message();
         $message->setBody($text);
         $message->setRecipients([$number]);
-        $message->setOriginator("Texteller");
+        $message->setOriginator($originator);
+        $message->setRoute($route);
 
         $response = self::$client->message->create($message);
 
         if ($response->wasSuccessful()) {
             $message = $response->toObject();
             $message_id = $message->getId();
-            return $message_id ? ["id" => $message_id] : false;
+            $used_originator = $message->getOriginator();
+            return $message_id
+                ? [
+                    "data" => ["id" => $message_id],
+                    "message_interface_number" => $used_originator,
+                ]
+                : false;
         } elseif ($response->serverError()) {
             TLR\tlr_write_log(
                 "Spryng: An error occurred while sending the message on Spryng servers."
@@ -126,115 +136,41 @@ class Base implements TLR\Interfaces\Gateway
      *
      * @return string|WP_Error
      */
-        public static function rest_delivery_callback(WP_REST_Request $request)
-        {
-            $method = $request->get_method();
-            $delivery_body = $request->get_body_params();
-            if (
-                "GET" === $method &&
-                !empty($delivery_body["ID"]) &&
-                !empty($delivery_body["STATUS"]) &&
-                in_array(
-                    $delivery_body["STATUS"],
-                    [10, 20]
-                )
-            ) {
-                $args = [
-                    "object_type" => "message",
-                    "statuses" => ["sent", "delivered", "failed", "pending"],
-                    "gateways" => ["spryng"],
-                    "field" => "ID",
-                    "gateway_data" => sanitize_text_field(
-                        $delivery_body["ID"]
-                    ),
-                ];
-                $message_query = new TLR\Object_Query($args);
-                $message_id = $message_query->get_messages(1);
-                $message = new TLR\Message($message_id[0]);
-                if ($message->get_id()) {
-                    $message->set_status($delivery_body["STATUS"] === 10 ? 'delivered' : 'failed');
-                    $message->save();
-                    return "success";
-                }
-            }
-            return new WP_Error(
-                "rest_invalid_message_data",
-                esc_html("Invalid message data"),
-                ["status" => 404]
-            );
-        }
+    public static function rest_delivery_callback(WP_REST_Request $request)
+    {
+        $method = $request->get_method();
+        $delivery_body = $request->get_query_params();
 
-//    /**
-//     * @param WP_REST_Request $request Current request.
-//     *
-//     * @return string|WP_Error
-//     */
-    //    public static function rest_receive_callback(WP_REST_Request $request)
-    //    {
-    //        $method = $request->get_method();
-    //        $message_body = $request->get_body_params();
-    //        $message_sid = isset($message_body["MessageSid"])
-    //            ? sanitize_text_field($message_body["MessageSid"])
-    //            : "";
-    //        $text = isset($message_body["Body"])
-    //            ? sanitize_text_field($message_body["Body"])
-    //            : "";
-    //        $to = isset($message_body["To"])
-    //            ? sanitize_text_field($message_body["To"])
-    //            : "";
-    //        $from = isset($message_body["From"])
-    //            ? sanitize_text_field($message_body["From"])
-    //            : "";
-    //        $member_id = TLR\tlr_get_member_id($from);
-    //
-    //        if ("POST" !== $method || !$from || !$to || !$message_sid) {
-    //            return new WP_Error(
-    //                "rest_invalid_message_data",
-    //                esc_html("Invalid message data"),
-    //                ["status" => 404]
-    //            );
-    //        } else {
-    //            if ($to !== self::get_interface_number("twilio-sms")) {
-    //                return new WP_Error(
-    //                    "rest_invalid_inbound_number",
-    //                    esc_html("Invalid inbound phone number"),
-    //                    ["status" => 404]
-    //                );
-    //            }
-    //            $token = self::get_encrypted_option(
-    //                "tlr_gateway_twilio_auth_token"
-    //            );
-    //            $url = get_rest_url(null, "texteller/v1/receive/twilio/");
-    //            $validator = new RequestValidator($token);
-    //
-    //            if (
-    //                $validator->validate(
-    //                    $request->get_header("x-twilio-signature"),
-    //                    $url,
-    //                    $message_body
-    //                )
-    //            ) {
-    //                $message = new TLR\Message();
-    //                $message->set_recipient($from);
-    //                $message->set_gateway("twilio");
-    //                $message->set_interface("twilio-sms");
-    //                $message->set_interface_number($to);
-    //                $message->set_gateway_data(["sid" => $message_sid]);
-    //                $message->set_content($text);
-    //                $message->set_status("received");
-    //                $message->set_trigger("tlr_inbound_message");
-    //                $message->set_member_id($member_id);
-    //                $message->save();
-    //                return "success";
-    //            } else {
-    //                return new WP_Error(
-    //                    "rest_forbidden_request",
-    //                    esc_html("Forbidden"),
-    //                    ["status" => 403]
-    //                );
-    //            }
-    //        }
-    //    }
+        if (
+            "GET" === $method &&
+            !empty($delivery_body["ID"]) &&
+            !empty($delivery_body["STATUS"]) &&
+            in_array($delivery_body["STATUS"], [10, 20])
+        ) {
+            $args = [
+                "object_type" => "message",
+                "statuses" => ["sent", "delivered", "failed", "pending"],
+                "gateways" => ["spryng"],
+                "field" => "ID",
+                "gateway_data" => sanitize_text_field($delivery_body["ID"]),
+            ];
+            $message_query = new TLR\Object_Query($args);
+            $message_id = $message_query->get_messages(1);
+            $message = new TLR\Message($message_id[0]);
+            if ($message->get_id()) {
+                $message->set_status(
+                    $delivery_body["STATUS"] == 10 ? "delivered" : "failed"
+                );
+                $message->save();
+                return "success";
+            }
+        }
+        return new WP_Error(
+            "rest_invalid_message_data",
+            esc_html("Invalid message data"),
+            ["status" => 404]
+        );
+    }
 
     public static function get_interfaces(): array
     {
@@ -263,6 +199,24 @@ class Base implements TLR\Interfaces\Gateway
     public static function is_interface_active(string $interface): bool
     {
         return true;
+    }
+
+    private static function get_originator(): string
+    {
+        $originator = get_option("tlr_gateway_spryng_originator", "Texteller");
+        return empty($originator) ? "Texteller" : $originator;
+    }
+
+    private static function get_route(): int
+    {
+        $route = get_option("tlr_gateway_spryng_route", "business");
+        if ($route === "business") {
+            return 5;
+        } elseif ($route === "economy") {
+            return 6;
+        } else {
+            return 5;
+        }
     }
 
     ///////////////////////////////////////////////////
@@ -317,6 +271,41 @@ class Base implements TLR\Interfaces\Gateway
                     "type" => "password",
                 ],
             ],
+            [
+                "id" => "tlr_gateway_spryng_originator",
+                "title" => __("Spryng originator", "texteller"),
+                "page" => "tlr_gateways",
+                "section" => "tlr_gateway_spryng",
+                "desc" => __(
+                    "The sender of the message for Spryng. Can be alphanumeric string (max. 11 characters) or phone number (max. 14 digits in E.164 format like 31612345678)",
+                    "texteller"
+                ),
+                "type" => "input",
+                "params" => [
+                    "type" => "text",
+                ],
+            ],
+            [
+                "id" => "tlr_gateway_spryng_route",
+                "title" => __("Spryng Route", "texteller"),
+                "page" => "tlr_gateways",
+                "section" => "tlr_gateway_spryng",
+                "field_args" => ["default" => "business"],
+                "desc" => __(
+                    "Spryng messaging route to be used while sending messages.",
+                    "texteller"
+                ),
+                "type" => "select",
+                "params" => [
+                    "options" => [
+                        "business" => __("Spryng business", "texteller"),
+                        "economy" => __("Spryng economy", "texteller"),
+                    ],
+                    "attribs" => [
+                        "style" => "width:200px;height:34px;",
+                    ],
+                ],
+            ],
         ];
         self::register_options($options);
     }
@@ -356,13 +345,13 @@ class Base implements TLR\Interfaces\Gateway
                 </div>
                 <div class="gateway-info-label-wrap">
                     <span><?= esc_html__(
-                        "Receive Endpoint",
+                        "Delivery Endpoint",
                         "texteller"
                     ) ?></span>
                 </div>
                 <div class="gateway-info-value-wrap">
                 <span><?= esc_html(
-                    get_rest_url(null, "texteller/v1/receive/spryng/")
+                    get_rest_url(null, "texteller/v1/delivery/spryng")
                 ) ?></span>
                 </div><?php } ?>
         </div>
